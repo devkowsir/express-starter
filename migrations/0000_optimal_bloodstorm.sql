6c96a8a79f7fc0aeacfbd6e6f456d7c7f41d7b05
Kawsar Ahmed <72383564+devkowsir@users.noreply.github.com>
2024-05-02 12:23:56 +0600

    removing migrations folder

--- migrations/0000_optimal_bloodstorm.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS "user" (
-	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"name" varchar(64) NOT NULL,
-	"email" varchar(64) NOT NULL,
-	"password" varchar(128),
-	"image" varchar(256),
-	CONSTRAINT "user_email_unique" UNIQUE("email")
-);
